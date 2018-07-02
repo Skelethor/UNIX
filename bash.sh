89cc455d6f12ce5b8ce191cae2c5bea8825ae0cf
Skelethor <regis.garofalo@gmail.com>
2018-07-02 21:29:51 +0200

    TD introduction au shell

---
+++ bash.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+$name = $1
+
+if [[ -z "$name" ]]
+
+then
+
+    echo "Coucou $USER"
+
+    echo ""
+
+    echo "Comment se déroule ton weekend ?"
+
+    echo ""
+
+else
+
+    echo "Coucou $name"
+
+    echo ""
+
+    echo "Comment se déroule ton weekend ?"
+
+    echo ""
+
+fin
